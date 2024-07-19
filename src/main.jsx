bc0408e833b5f621c0ed9eacf1b0dc6d277106c1
BenjGit <courtin-benjamin@outlook.fr>
2024-07-19 17:50:11 +0200

    hashrouter for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Layout } from './components/Layout'
-import Home from './pages/Home'
-import About from './pages/About'
-import Error from './pages/Error'
-import Logements from './pages/Logements'
-import { Navigate } from 'react-router-dom'
-import './globalStyle.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createHashRouter, Navigate, RouterProvider } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import "./globalStyle.css";
+import About from "./pages/About";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Logements from "./pages/Logements";
 
-
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     element: <Layout />,
     errorElement: <Navigate to="/404" />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/about',
+        path: "/about",
         element: <About />,
       },
       {
-        path: '/logements/:id',
+        path: "/logements/:id",
         element: <Logements />,
       },
       {
-        path: '/404',
+        path: "/404",
         element: <Error />,
       },
     ],
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
-
+  </React.StrictMode>
+);
